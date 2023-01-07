12b28a62295ff11959069cebcf4a97b875ba14c0
Max Starr <max@maxstarr.xyz>
2023-01-07 15:31:13 -0600

    cleanup and initial column

---
+++ src/lib/game/board.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { flip } from 'svelte/animate';
+	import Tile from './tile.svelte';
+
+	let tiles: Array<number> = Array.from({ length: 10 }, (_, i) => i);
+
+	function removeTile() {
+		console.log('removeTile');
+		// Remove the first element functionally
+		tiles = tiles.slice(1);
+	}
+</script>
+
+<section>
+	<!-- Create ten tiles for testing -->
+
+	<div class="board-containter">
+		{#each tiles as i (i)}
+			<div animate:flip={{ duration: 100 }}>
+				<Tile index={i} />
+			</div>
+		{/each}
+	</div>
+
+	<button on:click={removeTile}>Remove Tile</button>
+</section>
+
+<style>
+	.board-containter {
+		display: flex;
+		flex-direction: column-reverse;
+		height: 90vh;
+	}
+</style>
---
+++ src/lib/game/tile.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	const colors = [
+		'red',
+		'blue',
+		'green',
+		'yellow',
+		'orange',
+		'purple',
+		'pink',
+		'brown',
+		'black',
+		'white'
+	];
+
+	let color = colors[Math.floor(Math.random() * colors.length)];
+
+	export let index: number;
+</script>
+
+<!-- Create tile and set background color -->
+<div class="tile" style="background-color: {color}">
+	<p class="index">
+		{index}
+	</p>
+</div>
+
+<style>
+	.tile {
+		width: 100px;
+		height: 100px;
+	}
+
+	.index {
+		font-size: 2rem;
+	}
+</style>
